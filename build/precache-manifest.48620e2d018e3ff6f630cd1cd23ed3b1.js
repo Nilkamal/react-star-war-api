6b3093e6282e75d5a7e09c82a6609e5f6ef78b71
Nilkamal Shah <nilkamalsha75@gmail.com>
2019-01-06 18:20:03 +0530

    Build FOlder

---
+++ build/precache-manifest.48620e2d018e3ff6f630cd1cd23ed3b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "e41094e5176ab727d607",
+    "url": "/static/js/main.e41094e5.chunk.js"
+  },
+  {
+    "revision": "c31596ea3c2a837eae75",
+    "url": "/static/js/1.c31596ea.chunk.js"
+  },
+  {
+    "revision": "e41094e5176ab727d607",
+    "url": "/static/css/main.57f3a564.chunk.css"
+  },
+  {
+    "revision": "c31596ea3c2a837eae75",
+    "url": "/static/css/1.8184d59f.chunk.css"
+  },
+  {
+    "revision": "dac69df3491605eedd12c915b964cb3f",
+    "url": "/index.html"
+  }
+];
